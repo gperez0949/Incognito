189dc848911ebce9ec0a2c2d409e24423b4073ce
jpalmisano <jamesmpalmisano@gmail.com>
2015-02-02 16:04:28 -0500

    changed background color

--- SoftwareSpecs/src/softwareSpecs/Gui.java
+++ SoftwareSpecs/src/softwareSpecs/Gui.java
@@ -2,6 +2,7 @@
 //at 2/2/15
 package softwareSpecs;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
@@ -16,6 +17,7 @@ public class Gui extends JFrame {
 	public Gui() {
 		this.setSize(700, 500);//set size
 		this.setResizable(false);//can't resize
+		this.getContentPane().setBackground(Color.decode("#009900"));
 		//make it show up in the center of the screen each time
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation
@@ -27,19 +29,18 @@ public class Gui extends JFrame {
 		
 		JComponent panel1 = new JPanel();
 		tabbedPane.addTab("Step 1 Get Text File", panel1);
-		tabbedPane.setMnemonicAt(1, KeyEvent.VK_1);
+		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
 		JComponent panel2 = new JPanel();
 		tabbedPane.addTab("Step 2 Data extraction", panel2);
-		tabbedPane.setMnemonicAt(2, KeyEvent.VK_2);
+		tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 
 		JComponent panel3 = new JPanel();
 		tabbedPane.addTab("Step 3 Graphing and Export", panel3);
-		tabbedPane.setMnemonicAt(3, KeyEvent.VK_3);
+		tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
 
 		//by pressing alt and some number allows use to navigate the 
 		//tabs
-		tabbedPane.setMnemonicAt(2, KeyEvent.VK_4);
 		this.add(tabbedPane);
 		this.show();
 	}//end Gui
