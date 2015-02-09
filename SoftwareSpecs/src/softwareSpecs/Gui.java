289bed555d0fd7204e7c0ac89a44e46a1098ff80
Austin Nafziger <austinnafziger@Austins-MacBook-Pro.local>
2015-02-09 17:19:21 -0500

    Added the Data Stats Panel with pretty colors

--- SoftwareSpecs/src/softwareSpecs/Gui.java
+++ SoftwareSpecs/src/softwareSpecs/Gui.java
@@ -19,49 +19,55 @@ import javax.swing.border.EmptyBorder;
 public class Gui extends JFrame {
 
 	public Gui() {
-		this.setSize(700, 500);//set size
-		this.setResizable(false);//can't resize
-		this.getContentPane().setBackground(Color.decode("#001F48"));//FGCU Blue
-		//make it show up in the center of the screen each time
+		
+		this.setSize(720, 580);// set size
+		this.setResizable(false);// can't resize
+		this.getContentPane().setBackground(Color.decode("#001F48"));// FGCU// Blue
+		
+		// make it show up in the center of the screen each time
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setLocation
-			(dim.width/2-this.getSize().width/2,
-			 dim.height/2-this.getSize().height/2);
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height/ 2 - this.getSize().height / 2);
 		this.setTitle("Traffex Graphing Utility");
+		
 		JPanel wrapper = new JPanel();
 		wrapper.setLayout(new BorderLayout());
 		wrapper.setBackground(Color.decode("#001F48"));
 		wrapper.setBorder(new EmptyBorder(10, 10, 10, 10));
-		//make TabbedPane
-		JTabbedPane tabbedPane = new JTabbedPane();
-		tabbedPane.setBackground(Color.decode("#014D33"));//FGCU Green
-        
 		
-		/*@setMnemonicAt 
-		 * by pressing alt and some number allows use to navigate the 
-		tabs*/
+		// make TabbedPane
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.setBackground(Color.decode("#014D33"));// FGCU Green
+
+		/*
+		 * @setMnemonicAt by pressing alt and some number allows use to navigate
+		 * the tabs
+		 */
 		JComponent panel1 = new JPanel();
-		tabbedPane.addTab
-		("<html><font color=\"grey\">Step 1 Get Text File</font></html>", panel1);
+		tabbedPane.addTab("<html><font color=\"grey\">Step 1 Get Text File</font></html>",panel1);
 		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
 		JComponent panel2 = new JPanel();
-		tabbedPane.addTab
-		("<html><font color=\"grey\">Step 2 Data Extraction</font></html>", panel2);
+		tabbedPane.addTab("<html><font color=\"grey\">Step 2 Data Extraction</font></html>",panel2);
 		tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+		panel2.setSize(700, 500);
+		panel2.setVisible(true);
+		panel2.setLayout(null);
+		panel2.add(new DataSettings());
 
 		JComponent panel3 = new JPanel();
-		tabbedPane.addTab
-		("<html><font color=\"grey\">Step 3 Graphing and Export</font></html>", panel3);
+		tabbedPane.addTab("<html><font color=\"grey\">Step 3 Graphing and Export</font></html>",panel3);
 		tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
 
 		wrapper.add(tabbedPane);
 		this.add(wrapper);
 		this.show();
-	}//end Gui
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}// end Gui
 
 	public static void main(String[] args) {
+		
 		new Gui();
-	}//end main
-	
-}//end Gui
+	}// end main
+
+}// end Gui
