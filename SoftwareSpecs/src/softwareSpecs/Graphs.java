cf394a1dc579103994de62f3c895197d59ba0ee7
Austin Nafziger <austinnafziger@Austins-MacBook-Pro.local>
2015-02-23 18:35:45 -0500

    Switched the pie chart to a bar chart for the demo after spring break.

--- SoftwareSpecs/src/softwareSpecs/Graphs.java
+++ SoftwareSpecs/src/softwareSpecs/Graphs.java
@@ -4,18 +4,26 @@ package src.softwareSpecs;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JPanel;
 
+import javafx.scene.chart.NumberAxis;
 import org.jfree.JCommon;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.demo.BarChartDemo1;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DatasetChangeListener;
+import org.jfree.data.general.DatasetGroup;
 import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.xy.XYBarDataset;
 import org.jfree.util.Rotation;
 
 
@@ -29,17 +37,21 @@ public class Graphs extends JPanel {
         result.setValue("Linux", 29);
         result.setValue("Mac", 20);
         result.setValue("Windows", 51);
-		
-			JFreeChart chart = ChartFactory.createPieChart3D("test",          // chart title
-	            result,                // data
-	            true,                   // include legend
-	            true,
-	            false);
 
-	        PiePlot3D plot = (PiePlot3D) chart.getPlot();
-	        plot.setStartAngle(290);
-	        plot.setDirection(Rotation.CLOCKWISE);
-	        plot.setForegroundAlpha(0.5f);
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(7445, "JFreeSVG", "Warm-up");
+        dataset.addValue(24448, "Batik", "Warm-up");
+        dataset.addValue(4297, "JFreeSVG", "Test");
+        dataset.addValue(21022, "Batik", "Test");
+		
+			JFreeChart chart = ChartFactory.createBarChart("Test", "Time", "Amount of Cars", dataset);
+
+	        CategoryPlot plot = (CategoryPlot)chart.getPlot();
+        //NumberAxis rangeAxis = (NumberAxis)plot.getRangeAxis();
+        BarRenderer renderer = (BarRenderer)plot.getRenderer();
+        renderer.setDrawBarOutline(false);
+
+
 	        
 	        ChartPanel chartPanel = new ChartPanel(chart);
 	        
