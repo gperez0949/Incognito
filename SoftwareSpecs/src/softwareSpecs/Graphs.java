465bb03c3d2a84b1cc8d325e1ae4064ab5112064
Austin Nafziger <austinnafziger@Austins-MacBook-Pro.local>
2015-04-08 15:31:10 -0400

    It be magically delicious!

--- SoftwareSpecs/src/softwareSpecs/Graphs.java
+++ SoftwareSpecs/src/softwareSpecs/Graphs.java
@@ -1,57 +1,147 @@
 package src.softwareSpecs;
 
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.AxisState;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.axis.SymbolAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.chart.renderer.category.BarPainter;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.chart.renderer.category.StandardBarPainter;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RectangleEdge;
 import org.jfree.ui.RefineryUtilities;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import java.awt.image.ColorModel;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 import javax.swing.*;
+import javax.swing.colorchooser.ColorSelectionModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 
 public class Graphs extends JPanel{
 
-    private static int maximum = 0;
+    private static double maximum = 0;
+    private static Color barCol = Color.BLUE;
+    ChartPanel panel;
 
 	public Graphs(String chartTitle) throws IOException {
+
+        panel = update(chartTitle);
+
+        JPanel north = new JPanel();
+        JComboBox<String> events = new JComboBox<String>();
+        JComboBox<String> routes = new JComboBox<String>();
+        final JButton update = new JButton("Update");
+        JButton color = new JButton("Choose Color");
+
+        color.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                final JColorChooser colorChooser = new JColorChooser();
+
+                JFrame secondary = new JFrame();
+                secondary.setSize(500, 400);
+
+                secondary.add(colorChooser);
+
+                ColorSelectionModel model = colorChooser.getSelectionModel();
+                ChangeListener changeListener = new ChangeListener() {
+                    public void stateChanged(ChangeEvent changeEvent) {
+                        Color newForegroundColor = colorChooser.getColor();
+                        barCol = newForegroundColor;
+                    }
+                };
+                model.addChangeListener(changeListener);
+
+                secondary.setVisible(true);
+            }
+        });
+
+        update.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                    panel.getChart().setTitle("hello");
+                    panel.getChart().getCategoryPlot().getRenderer().setSeriesPaint(0, barCol);
+            }
+        });
+
+        north.add(events);
+        north.add(routes);
+        north.add(update);
+        north.add(color);
+
+        north.setBounds(0,0,900,50);
+
+        this.setVisible(true);
+        this.setSize(900, 700);
+        this.setLayout(null);
+
+        this.add(north);
+        this.add(panel);
+	}
+
+    private ChartPanel update(String chartTitle) throws IOException {
 
         JFreeChart barchart = ChartFactory.createBarChart(chartTitle,
                 "Time Interval",
                 "Percent Change",
                 createDataSet(),
                 PlotOrientation.VERTICAL,
-                true, true, false);
+                false, true, false);
+
+        CategoryPlot plot = barchart.getCategoryPlot();
+        BarRenderer renderer = (BarRenderer)plot.getRenderer();
+
+        String[] scale = new String[(int)maximum];
+
+        for(int i = 0; i< scale.length-1; i++){
+
+            scale[i] = Double.toString(i);
+        }
+
+        SymbolAxis rangeAxis = new SymbolAxis("", scale);
+        rangeAxis.setTickUnit(new NumberTickUnit(25));
+        rangeAxis.setAutoRange(false);
+        rangeAxis.setRange(0, maximum);
+        plot.setRangeAxis(rangeAxis);
+
+        renderer.setBarPainter(new StandardBarPainter());
+
+        renderer.setSeriesPaint(0, barCol);
 
         ChartPanel panel = new ChartPanel(barchart);
+
+        panel.setSize(900, 700);
+
+        panel.setBounds(0, 50, 900, 650);
+
         panel.setVisible(true);
-        panel.setSize(700, 500);
-        this.setVisible(true);
-        this.setSize(700, 500);
-        this.setLayout(null);
-
-        JPanel north = new JPanel();
-        JComboBox<String> events = new JComboBox<String>();
-        JComboBox<String> routes = new JComboBox<String>();
-        JButton update = new JButton("Update");
-
-        north.add(events);
-        north.add(routes);
-        north.add(update);
-
-        north.setBounds(0,0,700,50);
-        panel.setBounds(0, 50, 700, 450);
-
-        this.add(north);
-        this.add(panel);
-
-	}
+        
+        return panel;
+    }
 
     private CategoryDataset createDataSet() throws IOException {
 
@@ -60,13 +150,6 @@ public class Graphs extends JPanel{
         excelerator.Read();
         excelerator.Analyze("/Users/austinnafziger/Software/SoftwareSpecs/settings.dat");
 
-//        final String fiat = "FIAT";
-//        final String audi = "AUDI";
-//        final String ford = "FORD";
-//        final String speed = "Speed";
-//        final String millage = "Millage";
-//        final String userrating = "User Rating";
-//        final String safety = "safety";
 
         int start = excelerator.startTime;
         int end = excelerator.endTime;
@@ -75,38 +158,23 @@ public class Graphs extends JPanel{
 
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
-        for (int i = 0; i < end - start; i++) {
+        for (int i = 0; i < end - start+1; i++) {
 
             for(int j=0; j<4; j++){
 
-                dataset.addValue(data[i][j], Double.toString(data[i][0]), Double.toString(data[i][0]));
-                System.out.println(data[i][j]);
+                dataset.addValue(data[i][j], "0", Double.toString(data[i][0]));
+
             }
         }
-
-//        dataset.addValue( 1.0 , fiat , speed );
-//        dataset.addValue( 3.0 , fiat , userrating );
-//        dataset.addValue( 5.0 , fiat , millage );
-//        dataset.addValue( 5.0 , fiat , safety );
-//
-//        dataset.addValue( 5.0 , audi , speed );
-//        dataset.addValue( 6.0 , audi , userrating );
-//        dataset.addValue( 10.0 , audi , millage );
-//        dataset.addValue( 4.0 , audi , safety );
-//
-//        dataset.addValue( 4.0 , ford , speed );
-//        dataset.addValue( 2.0 , ford , userrating );
-//        dataset.addValue( 3.0 , ford , millage );
-//        dataset.addValue( 6.0 , ford , safety );
 
         return dataset;
     }
 
     private double[][] createTable(int s, int e){
 
-        double[][] temp = new double[e-s][4];
+        double[][] temp = new double[e-s+1][4];
 
-        for(int i = 0; i<e-s; i++){
+        for(int i = 0; i<e-s+1; i++){
 
             for(int j =0; j<4; j++){
 
@@ -116,11 +184,18 @@ public class Graphs extends JPanel{
 
                 }else if(j==3){
 
-                    temp[i][j] = (temp[i][1] - temp[i][2])/temp[i][2] * 100;
+                    temp[i][j] = (temp[i][1] - temp[i][2])/temp[i][2] * 100 + 70;
+                    //System.out.println(temp[i][j]);
+
+                    if(temp[i][j] > maximum){
+
+                       // System.out.println("Max changed");
+                        maximum = temp[i][j];
+                    }
 
                 }else{
 
-                    temp[i][j] = 20-j;
+                        temp[i][j]= 20+j;
                 }
             }
         }
